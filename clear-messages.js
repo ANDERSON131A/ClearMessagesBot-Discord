3da48326de1e8dd4c8e5e851c0d4ae565d94eaf2
ANDERSON131A <ANDERSON131A@users.noreply.github.com>
2017-05-01 02:55:28 -0700

    Update clear-messages.js

--- clear-messages.js
+++ clear-messages.js
@@ -12,7 +12,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 // Token of my bot
-const token = 'MjMxMjk3Mzg3OTU4NzYzNTIw.CtaOnA.eiqE_DIaS2hHW_c-k4jWLHR1Eu8';
+const token = 'MjcxNzc3MzY5OTk4NjIyNzMw.C8DSjg.XoC6GOsnHsvsNId7WK1l2gQG6Lc';
 
 bot.on('ready', () => {
   console.log('ClearMessagesBot is Ready!');
